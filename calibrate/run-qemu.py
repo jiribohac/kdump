8a37dc70db676e516b4754a1b8e2c8f31ccde419
Jiri Bohac <jbohac@suse.cz>
2025-06-17 18:08:25 +0200

    fix

--- calibrate/run-qemu.py
+++ calibrate/run-qemu.py
@@ -309,11 +309,11 @@ def run_qemu(bindir, params, initrd, elfcorehdr):
     if params['NET']:
         print("not implemented yet")
     else:
-        result = subprocess.run("dd if=/dev/zero of=/tmp/sda bs=1M seek=200", stdout=sys.stderr, stderr=sys.stderr, check=True)
+        result = subprocess.run(("dd", "if=/dev/zero", "of=/tmp/sda", "bs=1M", "seek=200"), stdout=sys.stderr, stderr=sys.stderr, check=True)
         if not result.returncode:
             print("dd result: ", result, file=sys.stderr)
 
-        result = subprocess.run("mkfs.ext3 /tmp/sda", stdout=sys.stderr, stderr=sys.stderr, check=True)
+        result = subprocess.run(("mkfs.ext3", "/tmp/sda"), stdout=sys.stderr, stderr=sys.stderr, check=True)
         if not result.returncode:
             print("mkfs result: ", result, file=sys.stderr)
 
@@ -330,11 +330,11 @@ def run_qemu(bindir, params, initrd, elfcorehdr):
         print("not implemented yet")
     else:
         os.mkdir("/tmp/mount")
-        result=subprocess.run("mount -o loop /tmp/sda /tmp/mount", stdout=sys.stderr, stderr=sys.stderr, check=True)
+        result=subprocess.run(("mount", "-o", "loop", "/tmp/sda", "/tmp/mount"), stdout=sys.stderr, stderr=sys.stderr, check=True)
         if not result.returncode:
             print("mount result: ", result, file=sys.stderr)
 
-        result=subprocess.run("cat /tmp/mount/var/crash/README", stdout=sys.stderr, stderr=sys.stderr, check=True)
+        result=subprocess.run(("cat", "/tmp/mount/var/crash/README"), stdout=sys.stderr, stderr=sys.stderr, check=True)
         if not result.returncode:
             print("cat README result: ", result, file=sys.stderr)
 
